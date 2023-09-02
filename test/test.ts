5fa0e578b0083102b153b32c9a3f18243d4b235e
8-Lambda-8 <j.wasle111@gmail.com>
2023-09-02 16:09:04 +0200

    added test

---
+++ test/test.ts
@@ -0,0 +1,13 @@
+import { syncthing, config } from "../src/syncthing";
+
+const options: config = {
+  apiKey: "Fumn9ume4NczkVnTgsagjmtbVpAnDizT",
+};
+
+const st = new syncthing(options);
+
+console.log("ping ");
+st.system.ping((res, err) => {
+  if (err) return;
+  console.log(res);
+});
